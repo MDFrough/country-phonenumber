4726e411127677d3da39513a7cf8578b85d76ac0
Muhammad Dursham <muhammad.dursham@cee-solutions.com>
2025-07-10 20:55:54 +0500

    fix: correct export statement for country.utils

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './flag.enum';
 export * from './country.model';
 export * from './country.data';
-export * from './country.utils'; // or wherever your class is
+export * from './country.utils';
